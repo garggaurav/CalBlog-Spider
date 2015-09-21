cba64c9534519bc3e0afb20161b1f2e1369f9151
Gaurav Garg <garggaurav@berkeley.edu>
2015-09-21 11:32:07 -0700

    Updated scraping.

--- dailyClog/spiders/articles.py
+++ dailyClog/spiders/articles.py
@@ -16,8 +16,10 @@ class MySpider(CrawlSpider):
         url = response.url
         title = response.xpath("//h2[@class='entry-title']/text()").extract()
         content = response.xpath("//div[@class='entry-content']").extract()
-        post["title"] = title
+        post["title"] = title[0].replace("\u2019", "'").replace("\u2018", "'")
         post["link"] = url
-        post["content"] = content
-        print "ANSWER: "+str([sentence + '.' for sentence in str(content).split('.') if 'You' in sentence])
-        return post
+        post["content"] = [sentence.replace("\u2019", "'").replace("\u2018", "'") + '.' for sentence in str(content).split('.') if 'You' in sentence]
+        if post["content"]:
+            return post
+        else:
+            return None
